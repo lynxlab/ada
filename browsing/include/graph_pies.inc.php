4bbb6bb3ddc08c19ef8c5f8eec78a77aae24e351
Giorgio Consorti <g.consorti@lynxlab.com>
2024-03-25 17:46:36 +0100

    fix to use jpgraph 4.4.2

--- browsing/include/graph_pies.inc.php
+++ browsing/include/graph_pies.inc.php
@@ -1,11 +1,19 @@
 <?php
-require_once realpath(dirname(__FILE__)) . '/../../config_path.inc.php';
+
+/**
+ *  Root dir relative path
+ */
+if (!defined('ROOT_DIR')) {
+  define('ROOT_DIR', realpath(dirname(__FILE__)) . '/../../');
+}
+require_once ROOT_DIR . '/config/config_main.inc.php';
+require_once ROOT_DIR . '/config/config_jsgraph.inc.php';
 
 include 'graph/jpgraph.php';
 include 'graph/jpgraph_pie.php';
-//import_request_variables("gP","");
-extract($_GET,EXTR_OVERWRITE,ADA_GP_VARIABLES_PREFIX);
-extract($_POST,EXTR_OVERWRITE,ADA_GP_VARIABLES_PREFIX);
+
+extract($_GET, EXTR_OVERWRITE, ADA_GP_VARIABLES_PREFIX);
+extract($_POST, EXTR_OVERWRITE, ADA_GP_VARIABLES_PREFIX);
 
 $nodes_percent_decode = urldecode($nodes_percent);
 
@@ -13,12 +21,12 @@ $nodes_percent_decode = urldecode($nodes_percent);
 $data = array($nodes_percent_decode, 100 - $nodes_percent_decode);
 
 // Crea un grafico a torta
-$graph = new PieGraph(300,200);
+$graph = new PieGraph(300, 200);
 $graph->SetShadow();
 
 // Set titolo
 $graph->title->Set('Nodi visitati');
-$graph->title->SetFont(FONT1_BOLD);
+$graph->title->SetFont(FF_FONT1, FS_BOLD);
 
 // Crea il grafico
 $p1 = new PiePlot($data);
