ce765dba97893e3a0c3de6b2986ec9755555ab83
Giorgio Consorti <g.consorti@lynxlab.com>
2024-03-26 13:00:54 +0100

    fixes for PHP 8.x, browsing/include/graph_pies.inc.php file

--- browsing/include/graph_pies.inc.php
+++ browsing/include/graph_pies.inc.php
@@ -3,35 +3,35 @@
 /**
  *  Root dir relative path
  */
-if (!defined('ROOT_DIR')) {
-  define('ROOT_DIR', realpath(dirname(__FILE__)) . '/../../');
-}
-require_once ROOT_DIR . '/config/config_main.inc.php';
-require_once ROOT_DIR . '/config/config_jsgraph.inc.php';
 
-include 'graph/jpgraph.php';
-include 'graph/jpgraph_pie.php';
+require_once realpath(dirname(__FILE__)) . '/../../' . '/config/config_main.inc.php';
+require_once realpath(dirname(__FILE__)) . '/../../' . '/config/config_jsgraph.inc.php';
+
+require_once 'graph/jpgraph.php';
+require_once 'graph/jpgraph_pie.php';
 
 extract($_GET, EXTR_OVERWRITE, ADA_GP_VARIABLES_PREFIX);
 extract($_POST, EXTR_OVERWRITE, ADA_GP_VARIABLES_PREFIX);
 
-$nodes_percent_decode = urldecode($nodes_percent);
+if (isset($nodes_percent)) {
+    $nodes_percent_decode = urldecode($nodes_percent);
 
-// Array dei dati
-$data = array($nodes_percent_decode, 100 - $nodes_percent_decode);
+    // Array dei dati
+    $data = [$nodes_percent_decode, 100 - $nodes_percent_decode];
 
-// Crea un grafico a torta
-$graph = new PieGraph(300, 200);
-$graph->SetShadow();
+    // Crea un grafico a torta
+    $graph = new PieGraph(300, 200);
+    $graph->SetShadow();
 
-// Set titolo
-$graph->title->Set('Nodi visitati');
-$graph->title->SetFont(FF_FONT1, FS_BOLD);
+    // Set titolo
+    $graph->title->Set('Nodi visitati');
+    $graph->title->SetFont(FF_FONT1, FS_BOLD);
 
-// Crea il grafico
-$p1 = new PiePlot($data);
-$p1->SetLegends(array('visitati'));
-$graph->Add($p1);
+    // Crea il grafico
+    $p1 = new PiePlot($data);
+    $p1->SetLegends(['visitati']);
+    $graph->Add($p1);
 
-// output
-$graph->Stroke();
+    // output
+    $graph->Stroke();
+}
