e9b2997619dded7afb25aa350e57011e3e6f88a1
Giorgio Consorti <g.consorti@lynxlab.com>
2024-04-03 15:46:18 +0200

    add switcher/include/functions.php

---
+++ switcher/include/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lynxlab\ADA\Switcher\Functions;
+
+function formatBytes($bytes, $precision = 2)
+{
+    if ($bytes > pow(1024, 3)) {
+        return round($bytes / pow(1024, 3), $precision) . "GB";
+    } elseif ($bytes > pow(1024, 2)) {
+        return round($bytes / pow(1024, 2), $precision) . "MB";
+    } elseif ($bytes > 1024) {
+        return round($bytes / 1024, $precision) . "KB";
+    } else {
+        return ($bytes) . "B";
+    }
+}
