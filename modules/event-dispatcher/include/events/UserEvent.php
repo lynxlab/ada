4ebcbb3bab13de86615d2e1a04cbd9192f99dbaa
Giorgio Consorti <g.consorti@lynxlab.com>
2025-09-08 10:28:05 +0200

    add UserEvent class

---
+++ modules/event-dispatcher/include/events/UserEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @package     event-dispatcher module
+ * @author      giorgio <g.consorti@lynxlab.com>
+ * @copyright   Copyright (c) 2025, Lynx s.r.l.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ * @version     0.1
+ */
+
+namespace Lynxlab\ADA\Module\EventDispatcher\Events;
+
+use Lynxlab\ADA\Module\EventDispatcher\ADAEventTrait;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * UserEvent class
+ */
+final class UserEvent extends GenericEvent
+{
+    use ADAEventTrait;
+
+    /**
+     * event own namespace
+     */
+    public const NAMESPACE = 'user';
+
+    /**
+     * The PRESAVE event occurs before the user is created (i.e. saved)
+     *
+     * This event allows you to add, remove or replace data
+     *
+     * @GenericEvent
+     *
+     * @var string
+     */
+    public const PRESAVE = self::NAMESPACE . '.presave';
+
+    /**
+     * The POSTSAVE event occurs after the user is created (i.e. saved)
+     *
+     * This event allows you to add actions after the user has been saved.
+     *
+     * @GenericEvent
+     *
+     * @var string
+     */
+    public const POSTSAVE = self::NAMESPACE . '.postsave';
+}
