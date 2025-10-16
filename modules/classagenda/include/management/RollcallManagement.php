172885de551720b9b79aa44f443c6f2254c17632
Giorgio Consorti <g.consorti@lynxlab.com>
2025-10-16 15:38:18 +0200

    fix bug giving empty rollcall history when empty eventData

--- modules/classagenda/include/management/RollcallManagement.php
+++ modules/classagenda/include/management/RollcallManagement.php
@@ -70,7 +70,7 @@ class RollcallManagement extends AbstractClassAgendaManagement
             case MODULES_CLASSAGENDA_DO_ROLLCALLHISTORY:
                 $htmlObj = CDOMElement::create('div', 'id:rollcallContainer');
                 if (
-                    empty($this->eventData) ||
+                    ($action == MODULES_CLASSAGENDA_DO_ROLLCALL && empty($this->eventData)) ||
                     !isset($this->id_course_instance) || is_null($this->id_course_instance) ||
                     strlen($this->id_course_instance) <= 0 || !is_numeric($this->id_course_instance) ||
                     !$this->isTutorOfInstance()
