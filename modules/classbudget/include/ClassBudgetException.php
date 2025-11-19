419b47838d91678dee92a80c0915004db6f258ea
Giorgio Consorti <g.consorti@lynxlab.com>
2025-11-19 18:05:57 +0100

    add ClassBudgetException

---
+++ modules/classbudget/include/ClassBudgetException.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * CLASSBUDGET MODULE.
+ *
+ * @package        classbudget module
+ * @author         Giorgio Consorti <g.consorti@lynxlab.com>
+ * @copyright      Copyright (c) 2015, Lynx s.r.l.
+ * @license        http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ * @link           classbudget
+ * @version        0.1
+ */
+
+namespace Lynxlab\ADA\Module\Classbudget;
+
+use Exception;
+
+/**
+ * ClassBudgetException class to handle custom exceptions
+ *
+ * @author giorgio
+ */
+class ClassBudgetException extends Exception
+{
+    // custom string representation of object
+    public function __toString(): string
+    {
+        return self::class . ": [{$this->code}]: {$this->message}\n";
+    }
+}
