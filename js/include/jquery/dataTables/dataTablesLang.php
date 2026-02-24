2297348c85c8a81a2dbc1125ac35337a2d0601f7
Giorgio Consorti <g.consorti@lynxlab.com>
2026-02-24 12:41:47 +0100

    add 'select' translations

--- js/include/jquery/dataTables/dataTablesLang.php
+++ js/include/jquery/dataTables/dataTablesLang.php
@@ -20,39 +20,45 @@ use function Lynxlab\ADA\Main\Output\Functions\translateFN;
 /**
  * Base config file
  */
-require_once realpath(__DIR__).'/../../../../config_path.inc.php';
+require_once realpath(__DIR__) . '/../../../../config_path.inc.php';
 
 /**
  * Clear node and layout variable in $_SESSION
  * $_SESSION was destroyed, so we do not need to clear data in session.
  */
-$allowedUsersAr = [AMA_TYPE_VISITOR, AMA_TYPE_STUDENT,AMA_TYPE_TUTOR, AMA_TYPE_AUTHOR, AMA_TYPE_SWITCHER, AMA_TYPE_ADMIN];
+$allowedUsersAr = [AMA_TYPE_VISITOR, AMA_TYPE_STUDENT, AMA_TYPE_TUTOR, AMA_TYPE_AUTHOR, AMA_TYPE_SWITCHER, AMA_TYPE_ADMIN];
 /**
  * Performs basic controls before entering this module
  */
 $trackPageToNavigationHistory = false;
-require_once ROOT_DIR.'/include/module_init.inc.php';
+require_once ROOT_DIR . '/include/module_init.inc.php';
 
 echo '{
-    "sEmptyTable":     "'.translateFN("Nessun dato presente nella tabella").'",
-    "sInfo":           "'.sprintf(translateFN("Vista da %s a %s di %s elementi"), '_START_', '_END_', '_TOTAL_').'",
-    "sInfoEmpty":      "'.sprintf(translateFN("Vista da %s a %s di %s elementi"), '0', '0', '0').'",
-    "sInfoFiltered":   "('.sprintf(translateFN("filtrati da %s elementi totali"), '_MAX_').')",
-    "sInfoPostFix":    "'."".'",
-    "sInfoThousands":  "'.",".'",
-    "sLengthMenu":     "'.sprintf(translateFN("Visualizza %s elementi"), '_MENU_').'",
-    "sLoadingRecords": "'.translateFN("Caricamento").'...",
-    "sProcessing":     "'.translateFN("Elaborazione").'...",
-    "sSearch":         "'.translateFN("filtra").':",
-    "sZeroRecords":    "'.translateFN("La ricerca non ha portato alcun risultato").'.",
+    "sEmptyTable":     "' . translateFN("Nessun dato presente nella tabella") . '",
+    "sInfo":           "' . sprintf(translateFN("Vista da %s a %s di %s elementi"), '_START_', '_END_', '_TOTAL_') . '",
+    "sInfoEmpty":      "' . sprintf(translateFN("Vista da %s a %s di %s elementi"), '0', '0', '0') . '",
+    "sInfoFiltered":   "(' . sprintf(translateFN("filtrati da %s elementi totali"), '_MAX_') . ')",
+    "sInfoPostFix":    "' . "" . '",
+    "sInfoThousands":  "' . "," . '",
+    "sLengthMenu":     "' . sprintf(translateFN("Visualizza %s elementi"), '_MENU_') . '",
+    "sLoadingRecords": "' . translateFN("Caricamento") . '...",
+    "sProcessing":     "' . translateFN("Elaborazione") . '...",
+    "sSearch":         "' . translateFN("filtra") . ':",
+    "sZeroRecords":    "' . translateFN("La ricerca non ha portato alcun risultato") . '.",
     "oPaginate": {
-        "sFirst":      "'.translateFN("Inizio").'",
-        "sPrevious":   "'.translateFN("Precedente").'",
-        "sNext":       "'.translateFN("Successivo").'",
-        "sLast":       "'.translateFN("Fine").'"
+        "sFirst":      "' . translateFN("Inizio") . '",
+        "sPrevious":   "' . translateFN("Precedente") . '",
+        "sNext":       "' . translateFN("Successivo") . '",
+        "sLast":       "' . translateFN("Fine") . '"
     },
     "oAria": {
-        "sSortAscending":  ": '.translateFN("attiva per ordinare la colonna in ordine crescente").'",
-        "sSortDescending": ": '.translateFN("attiva per ordinare la colonna in ordine decrescente").'"
-    }
+        "sSortAscending":  ": ' . translateFN("attiva per ordinare la colonna in ordine crescente") . '",
+        "sSortDescending": ": ' . translateFN("attiva per ordinare la colonna in ordine decrescente") . '"
+    },
+    "select": {
+       "rows": {
+            "_": "' . translateFN('%d righe selezionate') . '",
+            "0": "' . translateFN('Clicca una riga per selezionarla') . '",
+            "1": "' . translateFN('Una riga selezionata') . '"
+        }
 }';
